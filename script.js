9d3690a9b55e14c99393e707f1473e5d09e18fd4
Jorge Monge <jlo4321jl@gmail.com>
2021-05-12 21:54:39 -0600

    Add js logic to menu

---
+++ script.js
@@ -0,0 +1,5 @@
+function onClickMenu(){
+    document.getElementById("menu").classList.toggle("change")
+    document.getElementById("menu-ul").classList.toggle("change")
+    document.getElementById("menu-bg").classList.toggle("change-bg")
+}
