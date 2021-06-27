260abc5501151a9e596b5fc4bd370400d63f2dfe
Jorge Monge <jlo4321jl@gmail.com>
2021-06-27 12:07:59 -0600

    Add functionality of close menu when click outside

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+window.onload = function(){
+    document.onclick = function(e){
+        if(e.target.id == 'overlay'){
+            onClickMenu()
+        }
+    };
+};
 function onClickMenu(){
     document.getElementById("menu").classList.toggle("change")
     document.getElementById("menu-ul").classList.toggle("change")
     document.getElementById("menu-bg").classList.toggle("change-bg")
+    let overlay = document.getElementById('overlay')
+    overlayDisplay = window.getComputedStyle(overlay).display
+    if(overlayDisplay == "none"){
+        overlay.style.display = "block"
+    }else{
+        overlay.style.display = "none"
+    }
 }
