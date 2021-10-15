1dfc90524389f89c2ea54a41b57a23ec1f5a4be4
Jorge Monge <jlo4321jl@gmail.com>
2021-10-14 19:34:05 -0600

    Add homepage and css for the homepage

--- webpage/pages/index.tsx
+++ webpage/pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
 import Layout, { siteTitle } from '../components/layout'
+import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
@@ -8,6 +10,21 @@ const Home: NextPage = () => {
       <Head>
         <title>{siteTitle}</title>
       </Head>
+        {/* <main className="about-container"> */}
+        <main className={styles.main_container}>
+          <div className={styles.img_container}>
+                <Image className={styles.img_profile} src="/images/Monge.jpg" alt="Profile Photo" height={181} width={176}/>
+          </div>
+          <section className={styles.text_container}>
+            <div className={styles.text_description}>
+            <p>Hi, I'm a <a className="linkedin-a link" target="_blank" href="https://www.linkedin.com/in/monge1h/">Jorge.</a> 🥝<br/>
+        I’m a developer, I love to do cool <a className="this-website-a link" href="">projects</a> and 
+        <a className="this-website-a link" href="/blog.html"> writing</a> about them,I also create{" "}
+        <a className="yt-a link" target="_blank" href="https://www.youtube.com/monge1h">YouTube videos</a> about tech things and I love to listen
+        to <a target="_blank" className="spotify-a link" href="https://open.spotify.com/user/12125703988?si=a265bf6ed44d4c9c">music!</a></p>
+            </div>
+          </section>
+      </main>
     </Layout>
   )
 }
