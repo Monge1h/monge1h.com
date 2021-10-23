e3a90e36c4f1aaddc4103f902123c512b6240d05
Jorge Monge <jlo4321jl@gmail.com>
2021-10-23 13:54:24 -0600

    Add css module for a 404 page

--- webpage/pages/404.tsx
+++ webpage/pages/404.tsx
@@ -1,3 +1,14 @@
+import Image from 'next/image'
+import styles from '../styles/404.module.css'
+
 export default function Custom404(){
-	return <h1> Page not Not found</h1>
+	return (
+		<div>
+			<h1>404!</h1>
+			<Image src={'/images/water-drink.gif'} width='144' height='144'/>
+			<p>I don&apos;t know what you&apos;re looking for,
+				but drink some water
+			</p>
+		</div>
+	)
 }
