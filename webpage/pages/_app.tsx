f29247ae6790a6b0b25b93f163c31bbe5c81b977
Jorge Monge <jlo4321jl@gmail.com>
2022-10-15 12:38:47 -0600

    📈 :)

--- webpage/pages/_app.tsx
+++ webpage/pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Script from 'next/script'
+import GoogleAnalytics from "@bradgarropy/next-google-analytics"
 
 
 function MyApp({ Component, pageProps }: AppProps) {
    return (
         <>
+            <GoogleAnalytics measurementId={`${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
             <Component {...pageProps} />
         </>
     );
