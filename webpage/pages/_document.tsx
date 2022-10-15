b8d61afa9b5b3935bcdc9d7ca974accb197f70ef
Jorge Monge <jlo4321jl@gmail.com>
2022-10-15 09:53:32 -0600

    💚 fucking vercel

--- webpage/pages/_document.tsx
+++ webpage/pages/_document.tsx
@@ -17,19 +17,7 @@ class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
-  {/* Global Site Tag (gtag.js) - Google Analytics */}
-                      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
 
-            <Script strategy="lazyOnload">
-                {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                    page_path: window.location.pathname,
-                    });
-                `}
-            </Script>
 				</Head>
 
 				<body>
