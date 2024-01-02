e5889eb5a3459c539db06e04fa2c5b4e3b929797
Jorge Monge <jlo4321jl@gmail.com>
2024-01-02 11:13:48 -0600

    💄 Add mb to projects section

--- components/sections/Projects.tsx
+++ components/sections/Projects.tsx
@@ -7,7 +7,7 @@ export default function Projects() {
 	return (
 		<div className="tw-w-screen tw-flex tw-justify-center ">
 
-			<div className="tw-container tw-flex tw-justify-center tw-mt-[40px] md:tw-flex-col tw-flex-col sm:tw-items-center">
+			<div className="tw-container tw-flex tw-justify-center tw-mt-[40px] md:tw-flex-col tw-flex-col sm:tw-items-center tw-mb-[50px]">
 				<h3 className="tw-text-7xl tw-text-kiwi tw-text-center">Projects</h3>
 				<section className='tw-grid md:tw-grid-cols-3 tw-grid-cols-1  tw-container'>
 					<PostCard title='Test' id='1' date='-' post_description='jaja' post_image_alt='test' post_image_header='/projects/forkify/forkify.gif' redirectTo='/projects/forkify' key={1}/>
